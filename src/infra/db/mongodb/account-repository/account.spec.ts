79c5320e3909b0e9f9d73119f703ea0a9f2fb7f0
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-03-08 11:51:52 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { AccountMongoRepository } from '../account-repository/account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+
+    afterAll(async () => {
+      await MongoHelper.disconnect()
+    })
+
+    test('Should returns an account on success', async () => {
+      const sut = new AccountMongoRepository()
+      const account = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      })
+      expect(account).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
+})
