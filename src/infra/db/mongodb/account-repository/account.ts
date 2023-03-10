7cf26c1bf5c13b905fcec8ecaadafacaf1b51b54
bertoldoklinger <bertoldokbrj@gmail.com>
2023-03-09 22:13:27 -0300

    feat: ensure AccountMongoRepository returns an account with success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,10 +7,10 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository implements AddAccountRepository {
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
+
     const result = await accountCollection.insertOne(accountData)
     const id = result.insertedId.toString()
     const account = Object.assign({}, accountData, { id })
-    console.log(account)
     return account
   }
 }
